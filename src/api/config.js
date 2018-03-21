7a0d2085f24633bcc56194f3daec2d08fe5bda51
Santiago <anayasantiagohernan@gmail.com>
2018-03-21 15:44:32 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appname: 'platzimusic',
+    apiKey: '0a887abf8e0c296251fa3f5d57b76c9a',
+    secret: 'dd6008bcf6e4ef7973b1507aa48a4b51',
+    registeredTo: 'santianaya'
+}
+
+/* Application name	platzimusic
+API key	0a887abf8e0c296251fa3f5d57b76c9a
+Shared secret	dd6008bcf6e4ef7973b1507aa48a4b51
+Registered to	santianaya */
